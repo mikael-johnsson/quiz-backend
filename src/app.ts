a94a6d8b2d0977eb0e3d5261f5332e2efed3d3a7
mikael-johnsson <mikaeljohanjohnsson@gmail.com>
2026-03-12 10:47:55 +0100

    add try catch to mongoose connection

--- src/app.ts
+++ src/app.ts
@@ -39,6 +39,10 @@ app.use("/me", meRouter);
 
 const port = process.env.PORT || 3000;
 app.listen(port, async () => {
-  console.log(`Server is running on port ${port}`);
-  await mongoose.connect(uri, { dbName: "quiz" });
+  try {
+    await mongoose.connect(uri, { dbName: "quiz" });
+    console.log(`Server is running on port ${port}`);
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+  }
 });
